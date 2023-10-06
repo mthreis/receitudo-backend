e56a5606926ad2d2e54bda66633e5c5dd776826c
imokiswear <oreismatheus@gmail.com>
2023-10-06 16:40:35 -0300

    Remove cors declaration

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const cors = require("cors");
 app.use(express.json());
 app.use(express.static("build"));
 
-app.use(cors({ origin: "http://localhost:3000" }))
-
 console.log("random");
 
 app.use("/recipes", require("./routes/recipes"));
